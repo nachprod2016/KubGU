aed692b2b01204e6ee867da5954394010f760a95
Alex <93647915+nachprod2016@users.noreply.github.com>
2023-11-06 23:10:13 +0300

    Add files via upload

---
+++ 1st course/algorithms/lab11/Person.h
@@ -0,0 +1,13 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+struct Person
+{
+    char nameTeam[50];
+    char namePr[50];
+    char surnamePr[50];
+    char patronymic[50];
+    char age[50];
+};
+
+#endif
